0d1bad268336cf9505005cbf10cdfcdca96b54d6
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-06-25 07:43:48 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("commit to check githubwebhook trigger");
+        if (number == 0) throw new IllegalArgumentException("new commit to test github webhook");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
