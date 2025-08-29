6dcac4215271d2e63e15aa319e40b6c29369818b
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-08-29 10:45:44 +0530

    test commit on github

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("test commit for github pipeline proj");
+        if (number == 0) throw new IllegalArgumentException("test commit for github pipeline proj..");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
